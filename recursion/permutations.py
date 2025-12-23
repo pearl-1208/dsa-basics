e29597b240edee82901d36cef3a83725c51ada72
pearl-1208 <jainpearl1208@gmail.com>
2025-12-23 15:51:40 +0530

    Added recursive backtracking solution for permutations

---
+++ recursion/permutations.py
@@ -0,0 +1,18 @@
+def permutations(nums):
+    result = []
+
+    def backtrack(start):
+        if start == len(nums):
+            result.append(nums[:])
+            return
+
+        for i in range(start, len(nums)):
+            nums[start], nums[i] = nums[i], nums[start]
+            backtrack(start + 1)
+            nums[start], nums[i] = nums[i], nums[start]
+
+    backtrack(0)
+    return result
+
+
+print(permutations([1, 2, 3]))
