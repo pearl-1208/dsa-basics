cb6bd6909670a94be226ff37bd80d1026c627cc4
pearl-1208 <jainpearl1208@gmail.com>
2025-12-23 15:26:07 +0530

    Refactored variable names for better readability

--- arrays/two_sum.py
+++ arrays/two_sum.py
@@ -1,8 +1,14 @@
 def two_sum(nums, target):
-    mp = {}
-    for i, num in enumerate(nums):
-        if target - num in mp:
-            return [mp[target - num], i]
-        mp[num] = i
+    # Dictionary to store number and its index
+    num_index = {}
 
-print(two_sum([2, 7, 11, 15], 9))
+    for i, num in enumerate(nums):
+        complement = target - num
+        if complement in num_index:
+            return [num_index[complement], i]
+        num_index[num] = i
+
+    return []
+
+if __name__ == "__main__":
+    print(two_sum([2, 7, 11, 15], 9))
